21eaea1acf134e0900d8a43ed60d1a814afce6da
tommywiffler <tjwiff12@gmail.com>
2022-01-11 14:46:10 -0700

    added files and kube tmux

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -6,11 +6,13 @@ setopt PROMPT_SUBST
 
 set_prompt() {
 
+	#color codes to changes plugin colors: blue, red, magenta, green, yellow, red, cyan
+
 	# [
 	PS1="["
 
 	# Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-	PS1+="%{$fg_bold[blue]%}${PWD/#$HOME/~}%{$reset_color%}"
+	PS1+="%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
 
 	# Status Code
 	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
@@ -18,7 +20,7 @@ set_prompt() {
  	# Git
  	if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
  		PS1+=', '
- 		PS1+="%{$fg[magenta]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
+ 		PS1+="%{$fg_bold[blue]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
 		STATUS=$(git status --short | wc -l)
 		if [ $STATUS -gt 0 ]; then 
  			PS1+="%{$fg[green]%}+$(echo $STATUS | awk '{$1=$1};1')%{$reset_color%}"
@@ -29,7 +31,7 @@ set_prompt() {
 	# Timer: http://stackoverflow.com/questions/2704635/is-there-a-way-to-find-the-running-time-of-the-last-executed-command-in-the-shel
 	if [[ $_elapsed[-1] -ne 0 ]]; then
 		PS1+=', '
-		PS1+="%{$fg[yellow]%}$_elapsed[-1]s%{$reset_color%}"
+		PS1+="%{$fg[magenta]%}$_elapsed[-1]s%{$reset_color%}"
 	fi
 
 	# PID
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -9,14 +9,28 @@ source ~/dotfiles/zsh/plugins/fixls.zsh
 chpwd() ls
 
 
-autoload -U compinit && compinit
+autoload -Uz compinit && compinit
 
-source ~/dotfiles/zsh/plugins/zsh-vi-mode.plugin.zsh
+# Define PATH
+export PATH=~/.pyenv/shims:$PATH
+
+# zsh tab completion
+zstyle ':completion:*' menu select
+# case insensitive path-completion
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
+
+#Alias "clear" to work as "clc"
+alias clc="clear"
+alias python="python3"
+alias pip="pip3"
+
+#Source Files
+# source ~/dotfiles/zsh/plugins/zsh-vi-mode.plugin.zsh
 source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/dotfiles/env-vars.env
-source ~/dotfiles/zsh/lockbook.sh
 source ~/dotfiles/zsh/prompt.sh
+source ~/dotfiles/zsh/keybindings.sh
 zvm_after_init_commands+=('source ~/dotfiles/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh')
-zvm_after_init_commands+=('source /usr/share/fzf/key-bindings.zsh')
-zvm_after_init_commands+=('source /usr/share/fzf/completion.zsh')
+# zvm_after_init_commands+=('source /usr/share/fzf/key-bindings.zsh')
+# zvm_after_init_commands+=('source /usr/share/fzf/completion.zsh')
